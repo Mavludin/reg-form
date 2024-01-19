7bfe3cb5c7aa680fb4d5a1a6160a1f77f06d9120
Mavludin <imevlud@gmail.com>
2024-01-19 22:04:28 +0300

    pass parameters as an object to toggleErrorMessage

--- script.js
+++ script.js
@@ -5,108 +5,129 @@ const lastnameField = regForm.lastname;
 const emailField = regForm.email;
 const passwordField = regForm.password;
 
-const containsOnlyRussianLetters = (string) => {
-  return /^[а-я]+$/gi.test(string);
-};
+const modal = document.getElementById('modal');
 
-const isEmailValid = (string) => {
-  return /^[a-z][a-z._0-9]+@[a-z]+\.[a-z]{2,3}$/.test(string);
-};
+const containsOnlyRussianLetters = (string) => {
+  return /^[а-я]+$/i.test(string)
+}
+
+const isEmailValid = (email) => {
+  return /^[a-z][a-z._0-9]+@[a-z]+\.[a-z]{2,3}$/i.test(email)
+}
 
 const containsADigit = (string) => {
-  return /[0-9]/.test(string);
-};
+  return /[0-9]/.test(string)
+}
 
-const containsUppercaseLetter = (string) => {
-  return /[A-Z]/.test(string);
-};
+const containsUpperCaseLetter = (string) => {
+  return /[A-Z]/.test(string)
+}
 
-const containsLowercaseLetter = (string) => {
-  return /[a-z]/.test(string);
-};
+const containsLowerCaseLetter = (string) => {
+  return /[a-z]/.test(string)
+}
 
-const containsPunctuationMarks = (string) => {
-  return /[^A-Za-z0-9\s]/.test(string);
-};
+const containsPunctiationMark = (string) => {
+  return /[.,?\/#!$%\^&\*;:{}=\-_`~()]/.test(string)
+}
 
-const toggleErrorMessage = (condition, errorMessages, errorType) => {
+const isFormValid = () => {
+  const errorFields = document.querySelectorAll('.error');
+
+  return [...errorFields].every((errorField) => {
+    return window.getComputedStyle(errorField).display === 'none';
+  })
+}
+
+const toggleErrorMessage = ({ condition, errorMessages, errorType }) => {
   if (condition) {
-    errorMessages.namedItem(errorType).style.display = "none";
+    errorMessages.namedItem(errorType).style.display = 'none';
   } else {
-    errorMessages.namedItem(errorType).style.display = "block";
+    errorMessages.namedItem(errorType).style.display = 'block';
   }
-};
+}
 
-regForm.addEventListener("submit", (e) => {
+regForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // firstname validation
   const firstnameErrorMessages = firstnameField.parentElement.children;
   const firstnameFieldValue = firstnameField.value;
 
-  toggleErrorMessage(
-    firstnameFieldValue.length >= 3,
-    firstnameErrorMessages,
-    "length"
-  );
-  toggleErrorMessage(
-    containsOnlyRussianLetters(firstnameFieldValue),
-    firstnameErrorMessages,
-    "alphabet"
-  );
+  toggleErrorMessage({
+    condition: firstnameFieldValue.length >= 3,
+    errorMessages: firstnameErrorMessages,
+    errorType: 'length',
+  })
+
+  toggleErrorMessage({
+    condition: containsOnlyRussianLetters(firstnameFieldValue),
+    errorMessages: firstnameErrorMessages,
+    errorType: 'alphabet',
+  })
 
   // lastname validation
-  const lastnameFieldErrorMessages = lastnameField.parentElement.children;
+  const lastnameErrorMessages = lastnameField.parentElement.children;
   const lastnameFieldValue = lastnameField.value;
 
-  toggleErrorMessage(
-    lastnameFieldValue.length >= 3,
-    lastnameFieldErrorMessages,
-    "length"
-  );
-  toggleErrorMessage(
-    containsOnlyRussianLetters(lastnameFieldValue),
-    lastnameFieldErrorMessages,
-    "alphabet"
-  );
+  toggleErrorMessage({
+    condition: lastnameFieldValue.length >= 3,
+    errorMessages: lastnameErrorMessages,
+    errorType: 'length',
+  })
+
+  toggleErrorMessage({
+    condition: containsOnlyRussianLetters(lastnameFieldValue),
+    errorMessages: lastnameErrorMessages,
+    errorType: 'alphabet',
+  })
 
   // email validation
+  const emailErrorMessages = emailField.parentElement.children;
   const emailFieldValue = emailField.value;
-  const emailErrorMessage = emailField.nextElementSibling;
 
-  if (isEmailValid(emailFieldValue)) {
-    emailErrorMessage.style.display = "none";
-  } else {
-    emailErrorMessage.style.display = "block";
-  }
+  toggleErrorMessage({
+    condition: isEmailValid(emailFieldValue),
+    errorMessages: emailErrorMessages,
+    errorType: 'emailError',
+  })
 
   // password validation
-  const passwordFieldErrorMessages = passwordField.parentElement.children;
+  const passwordErrorMessages = passwordField.parentElement.children;
   const passwordFieldValue = passwordField.value;
 
-  toggleErrorMessage(
-    passwordFieldValue.length >= 8,
-    passwordFieldErrorMessages,
-    "length"
-  );
-  toggleErrorMessage(
-    containsADigit(passwordFieldValue),
-    passwordFieldErrorMessages,
-    "digit"
-  );
-  toggleErrorMessage(
-    containsUppercaseLetter(passwordFieldValue),
-    passwordFieldErrorMessages,
-    "uppercase"
-  );
-  toggleErrorMessage(
-    containsLowercaseLetter(passwordFieldValue),
-    passwordFieldErrorMessages,
-    "lowercase"
-  );
-  toggleErrorMessage(
-    containsPunctuationMarks(passwordFieldValue),
-    passwordFieldErrorMessages,
-    "alphanumeric"
-  );
-});
+  toggleErrorMessage({
+    condition: passwordFieldValue.length >= 8,
+    errorMessages: passwordErrorMessages,
+    errorType: 'length',
+  })
+
+  toggleErrorMessage({
+    condition: containsADigit(passwordFieldValue),
+    errorMessages: passwordErrorMessages,
+    errorType: 'digit',
+  })
+
+  toggleErrorMessage({
+    condition: containsUpperCaseLetter(passwordFieldValue),
+    errorMessages: passwordErrorMessages,
+    errorType: 'uppercase',
+  })
+
+  toggleErrorMessage({
+    condition: containsLowerCaseLetter(passwordFieldValue),
+    errorMessages: passwordErrorMessages,
+    errorType: 'lowercase',
+  })
+
+  toggleErrorMessage({
+    condition: containsPunctiationMark(passwordFieldValue),
+    errorMessages: passwordErrorMessages,
+    errorType: 'punctuation',
+  })
+
+  if (isFormValid()) {
+    modal.style.display = 'block';
+    regForm.style.display = 'none';
+  }
+})
